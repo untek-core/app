413849bf02f9035a8b0c627d28a040c038adcb6b
vitaliy <zndev@ya.ru>
2023-03-31 10:30:01 +0600

    upd

--- src/Libs/ZnCore.php
+++ src/Libs/ZnCore.php
@@ -71,7 +71,7 @@ class ZnCore
             return $containerConfigurator;
         });
 
-        $entityManagerConfigCallback = require __DIR__ . '/../../../../ntk-sandbox/packages/untek-domain/entity-manager/src/config/container.php';
+        $entityManagerConfigCallback = require __DIR__ . '/../../../../untek-model/entity-manager/src/config/container.php';
         call_user_func($entityManagerConfigCallback, $containerConfigurator);
 
         $eventDispatcherConfigCallback = require __DIR__ . '/../../../event-dispatcher/src/config/container.php';
